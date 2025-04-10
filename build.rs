a465826c1ed3f462b27e9554a704e2c3f09eecbc
Peter537 <pean@outlook.dk>
2025-04-10 14:35:21 +0200

    Create build.rs

---
+++ build.rs
@@ -0,0 +1,7 @@
+use winresource::WindowsResource;
+fn main() -> std::io::Result<()> {
+    if cfg!(target_os = "windows") {
+        WindowsResource::new().set_icon("img/icon.ico").compile()?;
+    }
+    Ok(())
+}
