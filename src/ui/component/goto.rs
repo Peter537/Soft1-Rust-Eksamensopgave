1172bf812f377f606b662851b82363ae17817c33
Peter537 <pean@outlook.dk>
2025-04-18 16:05:27 +0200

    Update goto.rs

--- src/ui/component/goto.rs
+++ src/ui/component/goto.rs
@@ -1,10 +1,11 @@
 use crate::ui::AppState;
 use druid::EventCtx;
 
-use crate::ui::Screen::{DriverScreen, TeamScreen};
+use crate::ui::Screen::{DriverScreen, RaceScreen, TeamScreen};
 
-use crate::database::teams::get_team_id_by_short_name;
 use crate::database::driver::get_driver_id_by_fullname;
+use crate::database::race::get_race_id_by_grandprix_name;
+use crate::database::teams::get_team_id_by_short_name;
 
 pub fn goto_driver() -> Box<dyn Fn(&str) -> Box<dyn Fn(&mut EventCtx, &mut AppState)>> {
     Box::new(|driver: &str| {
@@ -14,10 +15,8 @@ pub fn goto_driver() -> Box<dyn Fn(&str) -> Box<dyn Fn(&mut EventCtx, &mut AppSt
 
             let driver_id = get_driver_id_by_fullname(&driver).unwrap_or(-1);
 
-            _data.current_screen = DriverScreen {
-                driver_id,
-            };
-            
+            _data.current_screen = DriverScreen { driver_id };
+
             _ctx.request_update();
         })
     })
@@ -26,15 +25,28 @@ pub fn goto_driver() -> Box<dyn Fn(&str) -> Box<dyn Fn(&mut EventCtx, &mut AppSt
 pub fn goto_team() -> Box<dyn Fn(&str) -> Box<dyn Fn(&mut EventCtx, &mut AppState)>> {
     Box::new(|team: &str| {
         let team = team.to_string();
-        
+
         Box::new(move |_ctx: &mut EventCtx, _data: &mut AppState| {
             println!("Clicked team: {}", team);
 
             let team_id = get_team_id_by_short_name(&team).unwrap_or(-1);
 
-            _data.current_screen = TeamScreen {
-                team_id,
-            };
+            _data.current_screen = TeamScreen { team_id };
+
+            _ctx.request_update();
+        })
+    })
+}
+
+pub fn goto_race() -> Box<dyn Fn(&str) -> Box<dyn Fn(&mut EventCtx, &mut AppState)>> {
+    Box::new(|race: &str| {
+        let race = race.to_string();
+        Box::new(move |_ctx: &mut EventCtx, _data: &mut AppState| {
+            println!("Clicked race: {}", race);
+
+            let race_id = get_race_id_by_grandprix_name(&race).unwrap_or(-1);
+
+            _data.current_screen = RaceScreen { race_id };
 
             _ctx.request_update();
         })
