762ba18c2160e4cc4b247c612fe4b6f445177e75
Peter537 <pean@outlook.dk>
2025-05-18 10:05:29 +0200

    Update image_loader.rs

--- src/util/image_loader.rs
+++ src/util/image_loader.rs
@@ -3,31 +3,27 @@ use crate::util::appdata::get_mod_default_path;
 use druid::piet::ImageFormat;
 use druid::widget::{Image, Label};
 use druid::{Color, ImageBuf, Widget, WidgetExt};
-use image::load_from_memory;
-use std::fs;
+use image::open;
 use std::path::PathBuf;
 
 fn get_image_at(path: PathBuf) -> Box<dyn Widget<AppState>> {
     let image_path = get_mod_default_path().join(path);
-    if image_path.exists() {
-        let image_bytes = fs::read(image_path).expect("Failed to read image file");
-
-        let dyn_image = load_from_memory(&image_bytes).expect("Failed to decode image");
-
-        let rgba_image = dyn_image.to_rgba8();
-        let (width, height) = rgba_image.dimensions();
-
-        let image_buf = ImageBuf::from_raw(
-            rgba_image.into_raw(),
-            ImageFormat::RgbaSeparate,
-            width as usize,
-            height as usize,
-        );
-
-        Box::new(Image::new(image_buf).fix_size(200.0, 200.0))
-    } else {
-        Box::new(Label::new("Image not found").with_text_color(Color::RED))
+    if !image_path.exists() {
+        return Box::new(Label::new("Image not found").with_text_color(Color::RED));
     }
+
+    let dyn_image = open(&image_path).expect("Failed to open image");
+    let rgba_image = dyn_image.to_rgba8();
+    let (width, height) = rgba_image.dimensions();
+
+    let image_buf = ImageBuf::from_raw(
+        rgba_image.into_raw(),
+        ImageFormat::RgbaSeparate,
+        width as usize,
+        height as usize,
+    );
+
+    Box::new(Image::new(image_buf).fix_size(200.0, 200.0))
 }
 
 pub fn get_circuit(circuit_name: &str) -> Box<dyn Widget<AppState>> {
